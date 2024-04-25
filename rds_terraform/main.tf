0571509ad1672a7e339404b66205bd874765d540
Anuj Sharma <anuj_sharma401@yahoo.com>
2024-04-25 11:55:56 +0530

    Create main.tf

---
+++ rds_terraform/main.tf
@@ -0,0 +1,28 @@
+// rds.tf
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-"
+  ingress {
+    from_port   = 0
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "example" {
+  engine                 = "mysql"
+  db_name                = "example"
+  identifier             = "example"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 20
+  publicly_accessible    = true
+  username               = var.db-username
+  password               = var.db-password
+  vpc_security_group_ids = [aws_security_group.example.id]
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "example-db"
+  }
+}
