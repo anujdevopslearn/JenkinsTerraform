e5a48c742f7273bf78eb3c325d17af0b478a7deb
Anuj Sharma <anuj_sharma401@yahoo.com>
2026-06-14 11:17:14 +0530

    Update main.tf

--- vpc_terraform_2/main.tf
+++ vpc_terraform_2/main.tf
@@ -75,7 +75,7 @@ data "aws_ami" "latest-ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server*"]
   }
 
   filter {
